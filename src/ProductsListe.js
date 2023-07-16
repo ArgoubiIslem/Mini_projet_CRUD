6c8af7fc2a8d386cfdd91ddef08d44dae61a023b
unknown <argoubiislem5@gmail.com>
2023-07-16 21:51:52 +0200

    first commit

--- src/ProductsListe.js
+++ src/ProductsListe.js
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const ProductsListe = () => {
   const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
   const LoadDetail = (id) => {
     navigate('/products/detail/' + id);
   };
@@ -38,11 +39,16 @@ const ProductsListe = () => {
         console.log(err.message);
       });
   }, []);
+  const handelSearchTerm = (e) => {
+    let value = e.target.value;
+    setSearchTerm(value);
+  };
+  console.log(searchTerm);
   return (
     <div>
       <div class="flex justify-end my-4">
         <div class="max-w-2xl mx-auto">
-          <form class="flex items-center">
+          <form class="flex item-center justify-center ">
             <label for="simple-search" class="sr-only"></label>
             <div class="relative w-full">
               <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -62,9 +68,10 @@ const ProductsListe = () => {
               <input
                 type="text"
                 id="simple-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5    dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5    dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="recherche"
                 required
+                onChange={handelSearchTerm}
               />
             </div>
             <button
@@ -97,13 +104,18 @@ const ProductsListe = () => {
           </p>
           <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
         </div>
-        <Link to="products/create">
-          <button class="absolute right-20    h-12 w-48 overflow-hidden rounded-2xl bg-blue-500 text-lg font-bold text-white">
+
+        <Link
+          to="products/create"
+          className="md:max-w-2xl md:mx-auto text-center"
+        >
+          <button class="group relative h-12 w-48 overflow-hidden rounded-2xl bg-blue-500 text-lg font-bold text-white">
             Ajouter un produit
             <div class="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
           </button>
         </Link>
       </div>
+
       <table class="border-collapse w-3/4 mx-auto">
         <thead>
           <tr>
@@ -123,100 +135,104 @@ const ProductsListe = () => {
         </thead>
         <tbody>
           {productsData &&
-            productsData.map((item) => (
-              <tr key={item.id}>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                  <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
-                  {item.nom}
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                  <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
-                  {item.prix_unitaire}
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                  <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
-                  {item.quantite}
-                </td>
-                <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-                  <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-                    Actions
-                  </span>
-                  <td class="py-3 px-6 text-center">
-                    <div class="flex item-center justify-center">
-                      <div class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                        <a
-                          onClick={() => {
-                            LoadDetail(item.id);
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                            />
-                          </svg>
-                        </a>
-                      </div>
-                      <div class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                        <a
-                          onClick={() => {
-                            LoadEdit(item.id);
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                            />
-                          </svg>
-                        </a>
-                      </div>
-                      <div class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                        <a
-                          onClick={() => {
-                            Remove(item.id);
-                          }}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                            />
-                          </svg>
-                        </a>
-                      </div>
-                    </div>
+            productsData
+              .filter((item) => {
+                return item.nom.includes(searchTerm);
+              })
+              .map((item) => (
+                <tr key={item.id}>
+                  <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                    <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
+                    {item.nom}
                   </td>
-                </td>
-              </tr>
-            ))}
+                  <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                    <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
+                    {item.prix_unitaire}
+                  </td>
+                  <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                    <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase"></span>
+                    {item.quantite}
+                  </td>
+                  <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                    <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+                      Actions
+                    </span>
+                    <td class=" flex item-center justify-center">
+                      <div className="flex items-center">
+                        <div class="w-5 mr-2 transform hover:text-blue-500 hover:scale-110">
+                          <a
+                            onClick={() => {
+                              LoadDetail(item.id);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                              />
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                        <div class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110">
+                          <a
+                            onClick={() => {
+                              LoadEdit(item.id);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                        <div class="w-5 mr-2 transform hover:text-red-500 hover:scale-110">
+                          <a
+                            onClick={() => {
+                              Remove(item.id);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                              />
+                            </svg>
+                          </a>
+                        </div>
+                      </div>
+                    </td>
+                  </td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
